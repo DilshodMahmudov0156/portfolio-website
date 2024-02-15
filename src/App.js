0582457f3381676074c3d6e47566bcab6e5bebff
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-02-15 23:56:38 +0500

    I matched that bootstrap navbar for myself website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { useState, useEffect } from "react";
+import Section from "./parts/section";
+import Top from "./parts/top";
+import Footer from "./parts/footer";
 
 
 function App() {
 
     return(
         <div className="app">
-            <nav></nav>
+            <Top/>
+            <Section/>
+            <Footer/>
+
 
         </div>
     );
