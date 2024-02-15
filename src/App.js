f520bb48087a9c774fe7044a2a68a309c60e3b3a
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-02-15 23:39:36 +0500

    i have begin my website for portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { useState, useEffect } from "react";
+
+
+function App() {
+
+    return(
+        <div className="app">
+            <nav></nav>
+
+        </div>
+    );
+}
+
+export default App;
