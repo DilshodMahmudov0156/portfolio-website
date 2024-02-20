7bd2d13c607fa865b26e68933315da3b98140862
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-02-21 00:35:16 +0500

    i correceted header of my-portfolio

--- src/parts/header.js
+++ src/parts/header.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import myi_mage from "../images/photo_2024-02-20_23-58-04.jpg";
+import my_image from "../images/my-picture.png";
 
 function Header({}) {
     return (
         <header>
             <div className="container">
-                <div className="row">
+                <div className="row align-items-center justify-content-between">
                     <div className="col-xl-6">
                         <h1 className="header-blue">Hi there!</h1>
                         <h1 className="header-blue">I'm <span>Dilshod Mahmudov</span></h1>
                         <h3>I'm <span className="header-red">Front-End developer</span></h3>
                     </div>
-                    <div className="col-xl-6">
+                    <div className="col-xl-5">
                         <div className="my-image">
-                            <img src={myi_mage} alt=""/>
+                            <img src={my_image} alt=""/>
                         </div>
                     </div>
                 </div>
