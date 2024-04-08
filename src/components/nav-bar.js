826c6b775663e6723619244d05b1ec711de56ad0
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-04-08 23:42:04 +0500

    i fixed navbar mistakes

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -7,22 +7,15 @@ const forActive = ({isActive}) => {
 
 function NavBar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container">
+        <nav className="nav-bar d-flex justify-content-center align-items-center">
+            <div className="container d-flex justify-content-between align-items-center">
                 <a className="navbar-brand" href=""><h4 className="logo">D.Mahmudov</h4></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <ul className="navbar-nav">
-                        <NavLink to="/" className={forActive}>Home</NavLink>
-                        <NavLink to="/skills" className={forActive}>Skills</NavLink>
-                        <NavLink to="/projects" className={forActive}>Projects</NavLink>
-                        <NavLink to="/contact" className={forActive}>Contact</NavLink>
-                    </ul>
-                </div>
+                <ul className="d-flex mt-3">
+                    <NavLink to="/" className={forActive}>Home</NavLink>
+                    <NavLink to="/skills" className={forActive}>Skills</NavLink>
+                    <NavLink to="/projects" className={forActive}>Projects</NavLink>
+                    <NavLink to="/contact" className={forActive}>Contact</NavLink>
+                </ul>
             </div>
         </nav>
     );
--- src/parts/header.js
+++ src/parts/header.js
@@ -4,17 +4,15 @@ import my_image from "../images/my-picture.png";
 function Header({}) {
     return (
         <header>
-            <div className="container">
-                <div className="row align-items-center justify-content-between">
+            <div className="container mt-5">
+                <div className="row align-items-center justify-content-between mt-4">
                     <div className="col-xl-6">
                         <h1 className="header-blue">Hi there!</h1>
                         <h1 className="header-blue">I'm <span>Dilshod Mahmudov</span></h1>
                         <h3>I'm <span className="header-red">Front-End developer</span></h3>
                     </div>
                     <div className="col-xl-5">
-                        <div className="my-image">
-                            <img src={my_image} alt=""/>
-                        </div>
+                        <img src={my_image} alt="" className="img-fluid"/>
                     </div>
                 </div>
             </div>
